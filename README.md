5567a6075893707b69af79f7fb9375ac75ffad7f
oseasjs <oseasjs@gmail.com>
2022-01-03 15:07:24 -0300

    Updates

---
+++ README.md
@@ -0,0 +1,40 @@
+# Module Federation using WebPack 5 and React
+
+### Build Branches Status
+
+| Branch        | Build         | Coverage  |
+| ------------- |:-------------:| ---------:|
+| Develop       | ![CI](https://github.com/oseasjs/module-federation-react/workflows/CI/badge.svg?branch=develop) | [![codecov](https://codecov.io/gh/oseasjs/module-federation-react/branch/develop/graph/badge.svg)](https://codecov.io/gh/oseasjs/module-federation-react/branch/develop) |
+| Main          | ![CI](https://github.com/oseasjs/module-federation-react/workflows/CI/badge.svg?branch=main)  | [![codecov](https://codecov.io/gh/oseasjs/module-federation-react/branch/main/graph/badge.svg)](https://codecov.io/gh/oseasjs/module-federation-react/branch/main) |
+
+
+## This project was bootstrapped with:
+- [Node](https://nodejs.org/en/docs/);
+- [ReactJS](https://pt-br.reactjs.org/docs/getting-started.html);
+- [Webpack 5](https://webpack.js.org/concepts/);
+- [Redux](https://redux.js.org/introduction/getting-started);
+- [Github Pages](https://docs.github.com/en/pages);
+- [Github Actions](https://docs.github.com/pt/actions);
+- [Netlify](https://docs.netlify.com/);
+
+## Project Goal
+
+_TODO_
+
+## Running 
+
+_TODO_
+
+* Running Locally: http://localhost:9020 | http://localhost:9021 | http://localhost:9022
+* On Github Pages: https://oseasjs.github.io/module-federation-react/home/
+* On Vercel: https://module-federation-react.vercel.app/
+
+
+## Inspiration
+
+* Module Federation documentation: 
+  * https://webpack.js.org/concepts/module-federation/
+* Module Federation e-Book:
+  * https://module-federation.myshopify.com/
+* Practional videos by [Jack Herrington](https://www.jackherrington.com/micro-frontends/):
+  * https://www.youtube.com/playlist?list=PLNqp92_EXZBLr7p7hn6IYa1YPNs4yJ1t1
---
+++ applications/home/webpack.config.js
@@ -0,0 +1,86 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const deps = require("./package.json").dependencies;
+
+const getUrl = (env) => {
+
+  if (env.github) {
+    return 'https://oseasjs.github.io/module-federation-react/home/'
+  }
+  else if (env.vercel) {
+    return 'https://module-federation-react.vercel.app/'
+  }
+  else {
+    return 'http://localhost:9020/'
+  }
+  
+}
+
+module.exports = (_, argv) => ({
+  output: {
+    publicPath: getUrl(argv.env),
+  },
+  resolve: {
+    extensions: [".jsx", ".js", ".json"],
+  },
+  devServer: {
+    port: 9020,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "home",
+      filename: "remoteEntry.js",
+      remotes: {
+        home: "home@http://localhost:9020/remoteEntry.js",
+        checkout: "checkout@http://localhost:9021/remoteEntry.js",
+        search: "search@http://localhost:9022/remoteEntry.js",
+      },
+      exposes: {
+        "./Home": "./src/HomeContent",
+        "./Frame": "./src/Frame",
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+});
